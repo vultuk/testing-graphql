6c9550b3f1c78b94becaceeebc9db1c3d3eb8240
Simon Skinner <vultuk28@gmail.com>
2022-02-07 13:56:22 +0000

    Converted over to use Express

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
-import { ApolloServer } from 'apollo-server';
+import { ApolloServer } from 'apollo-server-express';
+import express from 'express';
+import http from 'http';
 import * as books from './books';
 import * as shops from './shops';
 
-const items: { resolvers; typeDefs }[] = [books, shops];
+const startServer = async () => {
+  const items: { resolvers; typeDefs }[] = [books, shops];
 
-const server = new ApolloServer({
-  typeDefs: items.map((i) => i.typeDefs),
-  resolvers: items.map((i) => i.resolvers),
-});
+  const app = express();
+  const httpServer = http.createServer(app);
 
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  const server = new ApolloServer({
+    typeDefs: items.map((i) => i.typeDefs),
+    resolvers: items.map((i) => i.resolvers),
+  });
+
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: '/',
+  });
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:4000`);
+};
+
+startServer();
