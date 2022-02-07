871c7e4663bf3f68b4e6d589b0cf8f18fe40f51f
Simon Skinner <vultuk28@gmail.com>
2022-02-07 11:36:03 +0000

    Initial work

---
+++ src/data.ts
@@ -0,0 +1,16 @@
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+    rating: 10,
+    description: 'Is a book',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+    rating: 8,
+    description: 'Is not a swede',
+  },
+];
+
+export default books;
